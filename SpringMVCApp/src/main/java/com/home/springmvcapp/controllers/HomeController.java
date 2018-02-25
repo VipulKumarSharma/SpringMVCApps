af661c015af49f5863838d2fae0f3fb5f22e8b97
VipulKumarSharma <tysonvks@gmail.com>
2018-02-25 19:51:31 +0530

    Learnings upto model passing and fetching it's properties.

---
+++ SpringMVCApp/src/main/java/com/home/springmvcapp/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.home.springmvcapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.home.springmvcapp.data.entities.Project;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String goHome(Model model){
+		
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by NASA");
+		
+		model.addAttribute("currentProject", project);
+		
+		return "home";
+	}
+}
